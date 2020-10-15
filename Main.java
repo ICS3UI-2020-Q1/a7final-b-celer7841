8a773d2784c2e77d4db60f15c42595ae7b11fb0b
celer7841 <celer7841@wrdsb.ca>
2020-10-15 05:48:14 +0000

    finised

--- Main.java
+++ Main.java
@@ -9,7 +9,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    System.out.println(isNiven(336)); 
+  }
+
+  public static boolean isNiven (int number){
+    int digitTotal = 0;
+    int temp = Math.abs(number);
+    while (temp > 0){
+      digitTotal = digitTotal + (temp % 10);
+      temp = temp /10;
+    }
+    System.out.println (digitTotal);
+    if(number % digitTotal ==0){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
